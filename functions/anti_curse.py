bf5e3cf3029f828d7153e0d2e96fea3bf49603f4
Tubication0812 <93110471+Tubication0812@users.noreply.github.com>
2021-11-22 16:22:59 +0700

    Update anti_curse.py

--- functions/anti_curse.py
+++ functions/anti_curse.py
@@ -1,4 +1,6 @@
 # Có vẻ có vài ông thích lách luật để chửi bậy trong server.
 # Với trí thông minh nhân tạo, chúc may mắn.
-
-
+'''Tubication:
+Ừ
+Nhưng nếu nó cũng dùng trí thông minh nhân tạo thì sao
+'''
