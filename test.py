e1bcc5ac8327e2e445b3e9dadb20520b65938941
SpikeBonjour <triphuongvasa@gmail.com>
2022-07-02 17:57:26 +0700

    fixes heroku

--- test.py
+++ test.py
@@ -1,36 +1,15 @@
-import sympy
+import asyncio
+import websockets
 
 
-def magic(expr: str):
-    x = sympy.symbols("x")
-    y = sympy.symbols("y")
-    n = expr.replace("x", "*x")
-    n = n.replace(" *x", "x")
-    n = n.replace("+*x", "+x")
-    n = n.replace("-*x", "-x")
-    n = n.replace("^", "**")
-    n = n.replace("***x", "**x")
-    n = n.replace("/*x", "/x")
-    n = n.replace("y", "*y")
-    n = n.replace(" *y", "y")
-    n = n.replace("+*y", "+y")
-    n = n.replace("-*y", "-y")
-    n = n.replace("***y", "**y")
-    n = n.replace("/*y", "/y")
-    n = n.replace("(*", "(")
-    n = n.replace(")(", ")*(")
-    if n[0] == "*":
-        n = n[1:]
-    print(n)
-    return eval(n, {"x": x, "y": y})
+async def echo(websocket):
+    async for message in websocket:
+        await websocket.send("never gonna give you up")
 
 
-sols = sympy.solve(magic("(x+3)(x-2)"), dict=True)
-output = ""
-# for s in sols:
-#     i = [str(z) for z in list(s.items())[0]]
-#     output += " = ".join(i) + "\n"
-#
-a = "".join([j[0] for j in [[" = ".join([str(z) for z in list(s.items())[0]]) + "\n"] for s in sols]])
-print(sols)
+async def main():
+    async with websockets.serve(echo, "localhost", 8765):
+        await asyncio.Future()  # run forever
+
+asyncio.run(main())
 
